06879f02ed3fd54c88f74e4242a978dddbee4b76
fachiri <fachrycooles@gmail.com>
2024-02-26 19:27:32 +0800

    fix: duplicate entry number

--- database/migrations/2024_01_11_211502_create_sub_items_table.php
+++ database/migrations/2024_01_11_211502_create_sub_items_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('sub_items', function (Blueprint $table) {
             $table->id();
             $table->uuid('uuid')->unique();
-            $table->integer('number')->unique();
+            $table->integer('number');
             $table->tinyInteger('is_pinjamable')->default(0);
             $table->date('entry_date');
             $table->foreignId('item_id')->constrained('items');
