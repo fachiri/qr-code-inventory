f6c93059a3a66e1f8bfcf856f0bd30b81d1e89d1
Mohamad Rafiq Mato <rafikmato@gmail.com>
2024-03-01 17:33:56 +0800

    fix tampilan

--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -75,7 +75,7 @@
 				</div>
 				<div class="col-6">
 					<div class="pl-0 col-12">
-						<div class="card shadow-lg">
+						<div class="card shadow-lg text-dark">
 							<div class="card-header">
 							<h4>Riwayat Peminjam</h4>
 							<div class="card-header-action">
@@ -126,5 +126,58 @@
 					</div>
 				</div>
 			</div>
+			<div class="card-header shadow-lg text-dark">
+				<h4>Kategori Barang</h4>
+			</div>
+			<div class="card-body shadow-lg text-dark">
+				<div class="mb-4">
+				<div class="text-small float-right font-weight-bold text-muted">2,100</div>
+				<div class="font-weight-bold mb-1">Google</div>
+				<div class="progress" data-height="3">
+					<div class="progress-bar" role="progressbar" data-width="80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+				</div>                          
+				</div>
+
+				<div class="mb-4">
+				<div class="text-small float-right font-weight-bold text-muted">1,880</div>
+				<div class="font-weight-bold mb-1">Facebook</div>
+				<div class="progress" data-height="3">
+					<div class="progress-bar" role="progressbar" data-width="67%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+				</div>
+				</div>
+
+				<div class="mb-4">
+				<div class="text-small float-right font-weight-bold text-muted">1,521</div>
+				<div class="font-weight-bold mb-1">Bing</div>
+				<div class="progress" data-height="3">
+					<div class="progress-bar" role="progressbar" data-width="58%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+				</div>
+				</div>
+
+				<div class="mb-4">
+				<div class="text-small float-right font-weight-bold text-muted">884</div>
+				<div class="font-weight-bold mb-1">Yahoo</div>
+				<div class="progress" data-height="3">
+					<div class="progress-bar" role="progressbar" data-width="36%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+				</div>
+				</div>
+
+				<div class="mb-4">
+				<div class="text-small float-right font-weight-bold text-muted">473</div>
+				<div class="font-weight-bold mb-1">Kodinger</div>
+				<div class="progress" data-height="3">
+					<div class="progress-bar" role="progressbar" data-width="28%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+				</div>
+				</div>
+
+				<div class="mb-4">
+				<div class="text-small float-right font-weight-bold text-muted">418</div>
+				<div class="font-weight-bold mb-1">Multinity</div>
+				<div class="progress" data-height="3">
+					<div class="progress-bar" role="progressbar" data-width="20%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+				</div>
+				</div>
+			</div>
+
 		</div>
 @endsection
