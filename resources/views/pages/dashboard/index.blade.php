8672265b8aa64ffd9c3d41db122b9493b82a9de5
Mohamad Rafiq Mato <rafikmato@gmail.com>
2024-03-01 19:42:25 +0800

    fix tampilan

--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -70,7 +70,62 @@
 									</div>
 								</div>
 							</div>
-						</div>     
+						</div>
+						<div class="col-lg-12 col-md-12 col-sm-12 col-12">
+							<div class="card">
+								<div class="card-header text-dark shadow-lg">
+									<h5>Kategori Barang</h5>
+								</div>
+								<div class="card-body text-dark shadow-lg">
+									<div class="mb-4">
+										<div class="text-small float-right font-weight-bold text-muted">2,100</div>
+											<div class="font-weight-bold mb-1">Google</div>
+												<div class="progress" data-height="3">
+													<div class="progress-bar" role="progressbar" data-width="80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+												</div>                          
+										</div>
+									<div class="mb-4">
+										<div class="text-small float-right font-weight-bold text-muted">1,880</div>
+											<div class="font-weight-bold mb-1">Facebook</div>
+											<div class="progress" data-height="3">
+												<div class="progress-bar" role="progressbar" data-width="67%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+											</div>
+									</div>
+					
+									<div class="mb-4">
+									<div class="text-small float-right font-weight-bold text-muted">1,521</div>
+									<div class="font-weight-bold mb-1">Bing</div>
+									<div class="progress" data-height="3">
+										<div class="progress-bar" role="progressbar" data-width="58%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+									</div>
+									</div>
+					
+									<div class="mb-4">
+									<div class="text-small float-right font-weight-bold text-muted">884</div>
+									<div class="font-weight-bold mb-1">Yahoo</div>
+									<div class="progress" data-height="3">
+										<div class="progress-bar" role="progressbar" data-width="36%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+									</div>
+									</div>
+					
+									<div class="mb-4">
+									<div class="text-small float-right font-weight-bold text-muted">473</div>
+									<div class="font-weight-bold mb-1">Kodinger</div>
+									<div class="progress" data-height="3">
+										<div class="progress-bar" role="progressbar" data-width="28%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+									</div>
+									</div>
+					
+									<div class="mb-4">
+									<div class="text-small float-right font-weight-bold text-muted">418</div>
+									<div class="font-weight-bold mb-1">Multinity</div>
+									<div class="progress" data-height="3">
+										<div class="progress-bar" role="progressbar" data-width="20%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+									</div>
+									</div>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 				<div class="col-6">
@@ -102,7 +157,20 @@
 										</div>
 									</td>
 									<td>
-										<a href="#" class="font-weight-600"><img src="assets/img/avatar/avatar-1.png" alt="avatar" width="30" class="rounded-circle mr-1"> Bagus Dwi Cahya</a>
+										<a href="#" class="font-weight-600"><img src="assets/img/avatar/avatar-1.png" alt="avatar" width="30" class="rounded-circle mr-1"> John Doe</a>
+									</td>
+									</tr>
+									<tr>
+									<td>
+										Introduction Laravel 5
+										<div class="table-links">
+										in <a href="#">Web Development</a>
+										<div class="bullet"></div>
+										<a href="#">View</a>
+										</div>
+									</td>
+									<td>
+										<a href="#" class="font-weight-600"><img src="assets/img/avatar/avatar-1.png" alt="avatar" width="30" class="rounded-circle mr-1"> John Doe</a>
 									</td>
 									</tr>
 									<tr>
@@ -115,7 +183,20 @@
 										</div>
 									</td>
 									<td>
-										<a href="#" class="font-weight-600"><img src="assets/img/avatar/avatar-1.png" alt="avatar" width="30" class="rounded-circle mr-1"> Bagus Dwi Cahya</a>
+										<a href="#" class="font-weight-600"><img src="assets/img/avatar/avatar-1.png" alt="avatar" width="30" class="rounded-circle mr-1"> John Doe</a>
+									</td>
+									</tr>
+									<tr>
+									<td>
+										Laravel 5 Tutorial - Installation
+										<div class="table-links">
+										in <a href="#">Web Development</a>
+										<div class="bullet"></div>
+										<a href="#">View</a>
+										</div>
+									</td>
+									<td>
+										<a href="#" class="font-weight-600"><img src="assets/img/avatar/avatar-1.png" alt="avatar" width="30" class="rounded-circle mr-1"> John Doe</a>
 									</td>
 									</tr>
 								</tbody>
@@ -128,60 +209,7 @@
 			</div>
 			<div class="row">
 				<div class="col-6">
-					<div class="card">
-						<div class="card-header text-dark shadow-lg">
-							<h5>Kategori Barang</h5>
-						</div>
-						<div class="card-body text-dark shadow-lg">
-							<div class="mb-4">
-							<div class="text-small float-right font-weight-bold text-muted">2,100</div>
-							<div class="font-weight-bold mb-1">Google</div>
-							<div class="progress" data-height="3">
-								<div class="progress-bar" role="progressbar" data-width="80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
-							</div>                          
-							</div>
-			
-							<div class="mb-4">
-							<div class="text-small float-right font-weight-bold text-muted">1,880</div>
-							<div class="font-weight-bold mb-1">Facebook</div>
-							<div class="progress" data-height="3">
-								<div class="progress-bar" role="progressbar" data-width="67%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-							</div>
-							</div>
-			
-							<div class="mb-4">
-							<div class="text-small float-right font-weight-bold text-muted">1,521</div>
-							<div class="font-weight-bold mb-1">Bing</div>
-							<div class="progress" data-height="3">
-								<div class="progress-bar" role="progressbar" data-width="58%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-							</div>
-							</div>
-			
-							<div class="mb-4">
-							<div class="text-small float-right font-weight-bold text-muted">884</div>
-							<div class="font-weight-bold mb-1">Yahoo</div>
-							<div class="progress" data-height="3">
-								<div class="progress-bar" role="progressbar" data-width="36%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-							</div>
-							</div>
-			
-							<div class="mb-4">
-							<div class="text-small float-right font-weight-bold text-muted">473</div>
-							<div class="font-weight-bold mb-1">Kodinger</div>
-							<div class="progress" data-height="3">
-								<div class="progress-bar" role="progressbar" data-width="28%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-							</div>
-							</div>
-			
-							<div class="mb-4">
-							<div class="text-small float-right font-weight-bold text-muted">418</div>
-							<div class="font-weight-bold mb-1">Multinity</div>
-							<div class="progress" data-height="3">
-								<div class="progress-bar" role="progressbar" data-width="20%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-							</div>
-							</div>
-						</div>
-					</div>
+					
 				</div>
 			</div>
 
