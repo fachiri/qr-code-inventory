9e7011d27e38934075fcea973cfbb158dbd11628
fachiri <fachrycooles@gmail.com>
2024-03-04 03:18:32 +0800

    feat: update dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Borrow;
+use App\Models\Category;
+use App\Models\SubItem;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('pages.dashboard.index');
+        $userCount = User::get()->count();
+        $categoryCount = Category::get()->count();
+        $itemCount = SubItem::get()->count();
+        $borrowCount = Borrow::get()->count();
+
+        $latestBorrows = Borrow::latest()->limit(4)->get();
+
+        $categories = Category::all();
+        $maxSubitemsCategory = null;
+        $maxSubitemsCount = 0;
+        foreach ($categories as $category) {
+            $subitemsCount = $category->items()->withCount('subitems')->get()->sum('subitems_count');
+            if ($subitemsCount > $maxSubitemsCount) {
+                $maxSubitemsCount = $subitemsCount;
+                $maxSubitemsCategory = $category;
+            }
+        }
+
+        return view('pages.dashboard.index', compact('userCount', 'categoryCount', 'itemCount', 'borrowCount', 'latestBorrows', 'categories', 'maxSubitemsCount'));
     }
 }
